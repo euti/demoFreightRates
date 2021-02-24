ee1219200412f31b797efe358fd28436ade06a16
Alberto Martínez Pizarro <alberto.martinezpizarro@eccenca.com>
2021-02-24 18:07:43 +0100

    add basic unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders', () => {
-  render(<App />);
+test('renders Search bar', () => {
+  const app = render(<App />);
+  expect(app.container.querySelector("#search")).toBeInTheDocument();
+  expect(app.container.querySelector("#origin")).toBeInTheDocument();
+  expect(app.container.querySelector("#destination")).toBeInTheDocument();
+  expect(app.container.querySelector("#freightMode")).toBeInTheDocument();
+  expect(app.container.querySelector("#pickupDate")).toBeInTheDocument();
+});
+
+test('renders without results', () => {
+  const app = render(<App />);
+  expect(app.container.querySelector("#dataTable")).not.toBeInTheDocument();
+});
+
+test('renders results after searching', () => {
+  const app = render(<App />);
+  fireEvent.click(app.container.querySelector("#searchButton"))
+  expect(app.container.querySelector("#dataTable")).toBeInTheDocument();
 });
